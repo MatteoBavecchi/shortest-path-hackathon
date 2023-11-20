a9c4d8f045fdf3ed5ba4908915ecb2b6fba4fe4d
Sebastiano <sebastiano.barravecchia@gmail.com>
2023-11-20 16:09:04 +0100

    add dijkstra with recharg and autonomy limit

--- backend/app/services/dijkstra_graph.rb
+++ backend/app/services/dijkstra_graph.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class DijkstraGraph
-  attr_reader :graph, :nodes, :previous, :distance #getter methods
+  attr_reader :graph, :nodes, :previous, :distance # getter methods
+
   INFINITY = 1 << 64
 
   def initialize
@@ -9,110 +10,100 @@ class DijkstraGraph
     @nodes = Set.new
   end
 
-# connect each node with target and weight
+  # connect each node with target and weight
   def connect_graph(source, target, weight)
-    if (!graph.has_key?(source))
-      graph[source] = {target => weight}
+    if !graph.has_key?(source)
+      graph[source] = { target => weight }
     else
       graph[source][target] = weight
     end
     nodes << source
   end
 
-# connect each node bidirectional
+  # connect each node bidirectional
   def add_edge(source, target, weight)
-    connect_graph(source, target, weight) #directional graph
-    connect_graph(target, source, weight) #non directed graph (inserts the other edge too)
+    connect_graph(source, target, weight) # directional graph
+    connect_graph(target, source, weight) # non directed graph (inserts the other edge too)
   end
 
-
-# based of wikipedia's pseudocode: http://en.wikipedia.org/wiki/Dijkstra's_algorithm
-
+  # based of wikipedia's pseudocode: http://en.wikipedia.org/wiki/Dijkstra's_algorithm
 
   def dijkstra(source)
-    @distance={}
-    @previous={}
-    nodes.each do |node|#initialization
-      @distance[node] = INFINITY #Unknown distance from source to vertex
-      @previous[node] = -1 #Previous node in optimal path from source
+    @distance = {}
+    @previous = {}
+    nodes.each do |node| # initialization
+      @distance[node] = INFINITY # Unknown distance from source to vertex
+      @previous[node] = -1 # Previous node in optimal path from source
     end
 
-    @distance[source] = 0 #Distance from source to source
+    @distance[source] = 0 # Distance from source to source
 
     queue = Set.new
     queue << source
 
-    while !queue.empty?
-      u = queue.min_by{|n| @distance[n]}
+    until queue.empty?
+      u = queue.min_by { |n| @distance[n] }
 
-      if (@distance[u] == INFINITY)
-        break
-      end
+      break if @distance[u] == INFINITY
 
       queue.delete(u)
 
       graph[u].keys.each do |vertex|
         alt = @distance[u] + graph[u][vertex]
 
-        if (alt < @distance[vertex])
-          @distance[vertex] = alt
-          @previous[vertex] = u  #A shorter path to v has been found
-          queue << vertex
-        end
+        next unless alt < @distance[vertex]
 
+        @distance[vertex] = alt
+        @previous[vertex] = u # A shorter path to v has been found
+        queue << vertex
       end
 
     end
-
   end
 
-
-# To find the full shortest route to a node
+  # To find the full shortest route to a node
 
   def find_path(dest)
-    if @previous[dest] != -1
-      find_path @previous[dest]
-    end
+    find_path @previous[dest] if @previous[dest] != -1
     @path ||= []
     @path << dest
   end
 
-
-# Gets all shortests paths using dijkstra
+  # Gets all shortests paths using dijkstra
 
   def all_shortest_paths(source)
-    @graph_paths=[]
+    @graph_paths = []
     @source = source
     dijkstra source
     nodes.each do |dest|
-      @path=[]
+      @path = []
 
       find_path dest
 
-      actual_distance=if @distance[dest] != INFINITY
-                        @distance[dest]
-                      else
-                        "no path"
-                      end
-      @graph_paths<< "Target(#{dest})  #{@path.join("-->")} : #{actual_distance}"
+      actual_distance = if @distance[dest] != INFINITY
+                          @distance[dest]
+                        else
+                          'no path'
+                        end
+      @graph_paths << "Target(#{dest})  #{@path.join('-->')} : #{actual_distance}"
     end
     @graph_paths
   end
 
   def shortest_path(source, dest)
-    @graph_paths=[]
+    @graph_paths = []
     @source = source
     dijkstra source
 
-    @path=[]
+    @path = []
 
     find_path dest
 
-    actual_distance=if @distance[dest] != INFINITY
-                      @distance[dest]
-                    else
-                      "no path"
-                    end
+    actual_distance = if @distance[dest] != INFINITY
+                        @distance[dest]
+                      else
+                        'no path'
+                      end
     @path
   end
 
@@ -132,42 +123,49 @@ class DijkstraGraph
     full_path
   end
 
+  def dijkstra_with_autonomy_and_recharge(start, goal, autonomy_limit, recharge_points = nil)
+    distances = Hash.new(Float::INFINITY)
+    distances[start] = 0
+    priority_queue = [[0, start, 0, 0]] # [total distance, current node, autonomy used, stops]
 
+    until priority_queue.empty?
+      priority_queue.sort! # Sort the queue based on the first element (total distance)
+      total_dist, current, autonomy_used, stops = priority_queue.shift
 
-def dijkstra_with_autonomy(start, goal, autonomy_limit) #TODO test and improve
-  distances = Hash.new(Float::INFINITY)
-  distances[start] = 0
-  priority_queue = [[0, start, 0]]   # [total distance, current node, autonomy used]
+      # Check if reached the goal
+      if current == goal
+        # Reached the goal, break out of the loop
+        break
+      end
 
-  while !priority_queue.empty?
-    total_dist, current, autonomy_used = priority_queue.pop
+      graph[current].each do |neighbor, weight|
+        new_autonomy = autonomy_used + weight
+        new_stops = stops
 
-    # Check if reached the goal
-    if current == goal
-      # Reached the goal, break out of the loop
-      break
-    end
+        # Check if autonomy limit is not exceeded
+        next unless new_autonomy <= autonomy_limit
 
-    graph[current].each do |neighbor, weight|
-      new_autonomy = autonomy_used + weight
-
-      # Check if autonomy limit is not exceeded
-      if new_autonomy <= autonomy_limit
         new_distance = distances[current] + weight
+
+        # Check if the neighbor is a recharge point
+        if recharge_points.nil? || recharge_points.include?(neighbor)
+          new_stops += 1
+          new_autonomy = 0 # Reset autonomy at recharge points
+        end
 
         if new_distance < distances[neighbor]
           distances[neighbor] = new_distance
-          priority_queue.push([new_distance, neighbor, new_autonomy])
+          priority_queue.push([new_distance, neighbor, new_autonomy, new_stops])
         end
       end
     end
-  end
 
-  # Now, distances hash contains the shortest distances from start to each node
-  # You can extract the shortest path from start to goal if it exists
-  if distances[goal] == Float::INFINITY
-    return nil  # No path found within autonomy limit
-  else
+    # Now, distances hash contains the shortest distances from start to each node
+    # You can extract the shortest path from start to goal if it exists
+    return nil if distances[goal] == Float::INFINITY
+
+    # No path found within autonomy limit
+
     # Reconstruct the path from the distances hash
     path = []
     current = goal
@@ -181,9 +179,6 @@ def dijkstra_with_autonomy(start, goal, autonomy_limit) #TODO test and improve
       end
     end
     path.unshift(start)
-    return path
+    path
   end
 end
-
-end
-
