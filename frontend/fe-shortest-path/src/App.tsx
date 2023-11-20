2f949fff38107b14163e890601c25c0a1ce1b491
Isidoro Allegretti <allegretti@extendi.it>
2023-11-20 18:21:34 +0100

    fe v2

---
+++ frontend/fe-shortest-path/src/App.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+function App() {
+  const [pointA, setPointA] = useState('');
+  const [pointB, setPointB] = useState('');
+
+  const handlePointAChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPointA(event.target.value);
+  };
+
+  const handlePointBChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPointB(event.target.value);
+  };
+
+  const handleFindRoute = () => {};
+
+  return (
+    <div className='flex bg-black'>
+      <div className='w-1/4 p-4 '>
+        <h1 className='text-white font-bold'>Shortest path hackathon</h1>
+        <div className='mt-24'>
+        <input
+          type='text'
+          value={pointA}
+          onChange={handlePointAChange}
+          placeholder='Point A'
+          className='w-full p-2 mb-2 border border-gray-300 rounded'
+        />
+        <input
+          type='text'
+          value={pointB}
+          onChange={handlePointBChange}
+          placeholder='Point B'
+          className='w-full p-2 mb-2 border border-gray-300 rounded'
+        />
+        <button
+          onClick={handleFindRoute}
+          className='w-full p-2 mt-6 bg-blue-500 text-white rounded'
+        >
+          Find Route
+        </button>
+        </div>
+      </div>
+      <div className='w-3/4 rounded-md'>
+      <MapContainer
+  className="full-height-map z"
+  c
+  zoom={6}
+  minZoom={3}
+  maxZoom={19}
+  maxBounds={[[-85.06, -180], [85.06, 180]]}
+  scrollWheelZoom={true}>
+  <TileLayer
+    attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors'
+    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+  />
+  {/* TODO: Add markers */}
+</MapContainer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,51 +17,44 @@ function App() {
   const handleFindRoute = () => {};
 
   return (
-    <div className='flex bg-black'>
-      <div className='w-1/4 p-4 '>
-        <h1 className='text-white font-bold'>Shortest path hackathon</h1>
-        <div className='mt-24'>
-          <input
-            type='text'
-            value={pointA}
-            onChange={handlePointAChange}
-            placeholder='Point A'
-            className='w-full p-2 mb-2 border border-gray-300 rounded'
-          />
-          <input
-            type='text'
-            value={pointB}
-            onChange={handlePointBChange}
-            placeholder='Point B'
-            className='w-full p-2 mb-2 border border-gray-300 rounded'
-          />
-          <button
-            onClick={handleFindRoute}
-            className='w-full p-2 mt-6 bg-blue-500 text-white rounded'
-          >
-            Find Route
-          </button>
-        </div>
-      </div>
-      <div className='w-3/4 rounded-md'>
-        <MapContainer
-          className='full-height-map'
-          zoom={6}
-          minZoom={3}
-          maxZoom={19}
-          maxBounds={[
-            [-85.06, -180],
-            [85.06, 180],
-          ]}
-          scrollWheelZoom={true}
+    <div className='relative'>
+      <div className='absolute top-10 left-10 mt-56 w-1/4 h-64 bg-black rounded-lg bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20  text-white p-4 z-10'>
+
+        <input
+          type='text'
+          value={pointA}
+          onChange={handlePointAChange}
+          placeholder='Punto di partenza'
+          className='w-full p-2 mb-2 mt-6 bg-black rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10    z-10 focus:outline-none focus:ring focus:border-yellow-400  '
+        />
+        <input
+          type='text'
+          value={pointB}
+          onChange={handlePointBChange}
+          placeholder='Punto di arrivo'
+          className='w-full p-2 mb-2 bg-black rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10  text-white   z-10 focus:outline-none focus:ring focus:border-yellow-400 '
+        />
+        <button
+          onClick={handleFindRoute}
+          className='w-full p-2 mt-6  mb-2 bg-white rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10  text-white  z-10 hover:bg-[#fed683] hover:text-black '
         >
-          <TileLayer
-            attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors'
-            url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
-          />
-          {/* TODO: Add markers */}
-        </MapContainer>
+          Calcola la rotta
+        </button>
       </div>
+
+      <MapContainer
+        center={[43, 10.69222]}
+        className='w-100% h-screen'
+        zoom={6}
+        minZoom={3}
+        maxZoom={19}
+        maxBounds={[[-85.06, -180], [85.06, 180]]}
+        scrollWheelZoom={true}
+        style={{ position: 'relative', zIndex: 1 }}>
+        <TileLayer
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
