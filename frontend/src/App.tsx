5ce4100edd066c14529f699e4cf723df0d69e73c
giacomosardo <giacomo.sardo98@gmail.com>
2023-11-21 17:09:36 +0100

    fix: delete stop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,10 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 const markerIconUrls = {
-  default: "https://images.emojiterra.com/twitter/v14.0/512px/1f535.png",
-  grayed: "grayed.png",
+  default: 'https://images.emojiterra.com/twitter/v14.0/512px/1f535.png',
+  grayed: 'grayed.png',
   highlighted:
-    "https://www.federmobili.it/wp-content/uploads/2021/01/pallino-arancione.png",
+    'https://www.federmobili.it/wp-content/uploads/2021/01/pallino-arancione.png',
 };
 
 const markerIconProps = {
@@ -23,7 +23,6 @@ const markerIconProps = {
   shadowSize: [0, 0], // dimensioni dell'ombra
   shadowAnchor: [0, 0], // punto dell'ombra che corrisponderà all'ombra del marker
 };
-
 
 interface IPoiProps {
   id: string;
@@ -117,10 +116,13 @@ function App() {
     });
   };
 
-  const handlePointRemove = (index: number) => {
-    setStops((prev) => {
-      return [...prev.filter((_, i) => i !== index)];
-    });
+  const handlePointRemove = (id: string) => {
+    const index: number = stops.indexOf(id);
+    if (index > -1) {
+      setStops((prev) => {
+        return stops.filter((_) => _ !== id);
+      });
+    }
   };
 
   const getIcon = (status: string) => {
@@ -143,69 +145,69 @@ function App() {
           ]),
         );
         setPathPoisOnMap(
-          pois.filter((poi) => data.path.find((item) => item === poi.id))
+          pois.filter((poi) => data.path.find((item) => item === poi.id)),
         );
-        if (typeof data.total_distance === "number")
+        if (typeof data.total_distance === 'number')
           setTotalDistance(Math.round(data.total_distance / 1000).toString());
-        else
-          setTotalDistance("");
+        else setTotalDistance('');
       });
     }
   }, [stops, pois]);
 
   return (
-    <div className="relative">
-      <div className="absolute top-2 right-5 bg-black  rounded-full bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20 px-[10px] text-white   z-10">
+    <div className='relative'>
+      <div className='absolute top-2 right-5 bg-black  rounded-full bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20 px-[10px] text-white   z-10'>
         <div
           onClick={() => setDarkMode(!darkMode)}
-          style={{ cursor: "pointer", fontSize: "35px", color: "white" }}
+          style={{ cursor: 'pointer', fontSize: '35px', color: 'white' }}
         >
-          {darkMode ? "☀" : "☾"}
+          {darkMode ? '☀' : '☾'}
         </div>
       </div>
-      <div className="absolute top-10 left-10 mt-56 w-1/4 bg-black rounded-lg bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20  text-white p-4 z-10">
+      <div className='absolute top-10 left-10 mt-56 w-1/4 bg-black rounded-lg bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20  text-white p-4 z-10'>
         {pois.length ? (
           <>
             <select
               onChange={(event) => handlePointChange(event, 0)}
-              id="countries"
-              className="w-full mt-2 p-2 mb-2 bg-black backdrop-filter backdrop-blur-xl bg-opacity-20 outline-none rounded-lg"
+              className='w-full mt-2 p-2 mb-2 bg-black backdrop-filter backdrop-blur-xl bg-opacity-20 outline-none rounded-lg'
             >
               <option>Punto di partenza</option>
               {pois
                 // .filter((poi) => !stops.find((stop) => poi.id === stop))
                 .map((poi) => (
-                  <option key={"0_".concat(poi.id)} value={poi.id}>
+                  <option key={'0_'.concat(poi.id)} value={poi.id}>
                     {poi.name}
                   </option>
                 ))}
             </select>
             {stops?.map((_, index) => (
-              <div className="flex-row items-center">
+              <div
+                key={`select_number_${index}`}
+                className='flex-row items-center'
+              >
                 <select
-                  key={`select_number_${index}`}
+                  value={stops.length > 1 ? stops[index + 1] : 'init'}
                   onChange={(event) => handlePointChange(event, index + 1)}
-                  id="countries"
-                  className="p-2 mb-2 bg-black backdrop-filter backdrop-blur-xl bg-opacity-20 outline-none rounded-lg"
+                  className='p-2 mb-2 bg-black backdrop-filter backdrop-blur-xl bg-opacity-20 outline-none rounded-lg'
                 >
-                  <option>Aggiungi fermata</option>
-                  {pois
-                    // .filter((poi) => !stops.find((item) => poi.id === item))
-                    .map((poi) => (
-                      <option key={`${index + 1}_${poi.id}`} value={poi.id}>
-                        {poi.name}
-                      </option>
-                    ))}
-                </select>{" "}
+                  <option value='init'>Aggiungi fermata</option>
+                  {pois.map((poi) => (
+                    <option key={`${index + 1}_${poi.id}`} value={poi.id}>
+                      {poi.name}
+                    </option>
+                  ))}
+                </select>{' '}
                 {index < stops.length - 1 && (
-                  <button onClick={() => handlePointRemove(index)}>Rimuovi</button>
+                  <button onClick={() => handlePointRemove(stops[index + 1])}>
+                    Rimuovi
+                  </button>
                 )}
               </div>
             ))}
             {totalDistance && <span>Distanza totale: {totalDistance} Km</span>}
           </>
         ) : (
-          <p className="pl-2">Loading data...</p>
+          <p className='pl-2'>Loading data...</p>
         )}
       </div>
 
@@ -213,7 +215,7 @@ function App() {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         center={[43, 10.69222]}
-        className="w-100% h-screen"
+        className='w-100% h-screen'
         zoom={6}
         minZoom={3}
         maxZoom={19}
@@ -227,13 +229,13 @@ function App() {
           setPoisOnMap(data);
         }}
         scrollWheelZoom={true}
-        style={{ position: "relative", zIndex: 1 }}
+        style={{ position: 'relative', zIndex: 1 }}
       >
         <TileLayer
           url={
             darkMode
-              ? "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-              : "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+              ? 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+              : 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
           }
         />
         {poisOnMap?.length &&
@@ -241,7 +243,7 @@ function App() {
             <Marker
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
-              icon={getIcon("default")}
+              icon={getIcon('default')}
               position={[point.latitude, point.longitude]}
               key={index}
             >
@@ -259,7 +261,7 @@ function App() {
             <Marker
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
-              icon={getIcon("highlighted")}
+              icon={getIcon('highlighted')}
               position={[point.latitude, point.longitude]}
               key={index}
             >
@@ -272,10 +274,11 @@ function App() {
               </Tooltip>
             </Marker>
           ))}
-        <Polyline pathOptions={{ color: "#6b7280" }} positions={polyline} />
+        <Polyline pathOptions={{ color: '#6b7280' }} positions={polyline} />
       </MapContainer>
     </div>
   );
 }
 
 export default App;
+
